1f4a0aa12705c3c77fb4616189fe3aa0e5fd5033
PeterDaveHello <hsu@peterdavehello.org>
2013-08-20 17:00:14 +0800

    support build dep only

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -x
 
-tmux rename-window "updating apt db"
-apt-get update
-
-tmux rename-window "upgrading packages"
-apt-get upgrade --force-yes -y
-
 REPO="dists/wheezy-sh4/main/binary-sh4"
 
 function build_sub_package()
@@ -41,9 +35,46 @@ function build_a_package()
     && cd -
 }
 
-for packages in $@
-do
-    build_a_package $packages
-done
+function build_a_package_dep()
+{
+    package=$1
+    echo $package;
+    $?=123
+    until [ "$?" -eq 0 ]
+    do
+        tmux rename-window "building-dep $package"
+        apt-get build-dep "$package" --force-yes -y 2>&1 | grep 'cannot be found' | cut -d ' ' -f 12 | build_sub_package
+        apt-get build-dep "$package" --force-yes -y 2>&1 | grep 'but it is not going to be installed' | cut -d ' ' -f 5 | build_sub_package
+        apt-get build-dep "$package" --force-yes -y 2>&1 | grep 'but it is not installable' | cut -d ' ' -f 5 | build_sub_package
+        apt-get build-dep "$package" --force-yes -y
+    done
+}
+
+if [ "$#" -lt 1 ]; then
+    exit
+elif [ "$#" -eq 1 ]; then
+    if [ "$1" == "--build-dep-only" ]; then
+        exit
+    fi
+fi
+
+tmux rename-window "updating apt db"
+apt-get update
+
+tmux rename-window "upgrading packages"
+apt-get upgrade --force-yes -y
+
+if [ "$1" == "--build-dep-only" ]; then
+    shift
+    for packages in $@
+    do
+        build_a_package_dep $packages
+    done
+else
+    for packages in $@
+    do
+        build_a_package $packages
+    done
+fi
 
 tmux rename-window "Done."
